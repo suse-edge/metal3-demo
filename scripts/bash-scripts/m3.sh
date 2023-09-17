42a1c774c0f6bb9bf0a50811d1a045ae99d7beae
dbw7 <90856056+dbw7@users.noreply.github.com>
2023-09-17 22:43:30 +0000

    update charts repo, change file name because of git-ignore

--- scripts/bash-scripts/m3.sh
+++ scripts/bash-scripts/m3.sh
@@ -25,7 +25,7 @@ python3 -m pip install ansible
 ssh-keygen -t ed25519 -C "m3-demo" -f ~/.ssh/id_ed25519 -N ""
 
 # Copy over pre-configured extra_vars file
-cp ~/metal3-demo/scripts/required-files/extra_vars.yml ~/metal3-demo/
+cp ~/metal3-demo/scripts/required-files/pre-configured_extra_vars.yml ~/metal3-demo/
 
 # Take the generated key and put it in extra_vars.yml
 PUBLIC_KEY_FILE="$HOME/.ssh/id_ed25519.pub"
