33d413eaf3341a0bf6c12250072d0a21cc8b089f
Steven <steven.hardy@suse.com>
2023-11-08 18:53:03 +0200

    Use a default extra_vars.yml
    To avoid the copying we can just use a default file, which can be
    overridden with a user copy if needed

--- scripts/bash-scripts/deploy_m3.sh
+++ scripts/bash-scripts/deploy_m3.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Copy over pre-configured extra_vars file
-cp ~/metal3-demo/scripts/required-files/pre-configured_extra_vars.yml ~/metal3-demo/extra_vars.yml
-
 # Run the playbook with the verbose flag
 ansible-playbook ~/metal3-demo/scripts/playbooks/m3-playbook.yaml -v
 
